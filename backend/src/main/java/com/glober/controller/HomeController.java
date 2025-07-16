149e7561ee0db40e20a7bbfc1fd4d05289697ac8
steliosgnl <stylianos.giannelos@gmail.com>
2025-07-16 23:54:26 +0300

    feat: Spring installation

---
+++ backend/src/main/java/com/glober/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.glober.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Glober API!";
+    }
+}

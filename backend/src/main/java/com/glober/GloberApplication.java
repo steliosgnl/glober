b565e5871b708ae31020453e2f7dbd916c19e860
steliosgnl <stylianos.giannelos@gmail.com>
2025-07-16 03:22:43 +0300

    Initial project setup: Spring Boot backend, Angular frontend, nations.sql

---
+++ backend/src/main/java/com/glober/GloberApplication.java
@@ -0,0 +1,13 @@
+package com.glober;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GloberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GloberApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/glober/GloberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glober;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GloberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
